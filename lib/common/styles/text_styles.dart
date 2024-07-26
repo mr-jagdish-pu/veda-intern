2a3caac4344acb1b7731a175fc425c9556a2a48f
jagdish <jagdissghimire@gmail.com>
2024-07-26 11:25:37 +0545

    chore: Most used text styles created separated

---
+++ lib/common/styles/text_styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+TextStyle get SimpleThinGrey =>
+    TextStyle(color: Colors.grey.shade700, fontSize: 13.5);
+TextStyle get HeadlineBlack =>
+    TextStyle(color: Colors.black87, fontSize: 14, fontWeight: FontWeight.w900);
+TextStyle get SimpleBoldBlack => TextStyle(
+    color: Colors.grey.shade700, fontSize: 13, fontWeight: FontWeight.w900);
