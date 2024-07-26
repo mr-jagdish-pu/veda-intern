55d74aeb8e90818cf6193a17ae6588a885917383
jagdish <jagdissghimire@gmail.com>
2024-07-26 11:26:34 +0545

    fix: some ui fixes

--- lib/features/news_display/presentation/ui/pages/news_headlines_page.dart
+++ lib/features/news_display/presentation/ui/pages/news_headlines_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jaggy/common/enums/news_coverage_enum.dart';
 import 'package:jaggy/common/extentions/spacer_ext.dart';
+import 'package:jaggy/common/styles/text_styles.dart';
 import 'package:jaggy/features/news_display/presentation/ui/pages/tabs/news_tab.dart';
 
 class NewsHeadlinesPage extends StatefulWidget {
@@ -12,40 +13,68 @@ class NewsHeadlinesPage extends StatefulWidget {
 }
 
 class _NewsHeadlinesPageState extends State<NewsHeadlinesPage> {
+  int defaultTab = 0;
   @override
   Widget build(BuildContext context) {
+    final tTheme = Theme.of(context).textTheme;
     return DefaultTabController(
       length: 8,
-      initialIndex: 0,
-
+      initialIndex: defaultTab,
       child: Scaffold(
         appBar: AppBar(
           title: Image.asset('assets/img/khabar.png'),
-          actions: [IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.bell))],
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bell))
+          ],
         ),
-        body: Column(children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,children: [Text('Latest'), Text('See all')],),
-          ),
-          20.ht,
-          TabBar(tabs: [
-            Tab(text: 'All'),
-            Tab(text: 'Sports'),
-            Tab(text: 'Politics'),
-            Tab(text: 'Business'),
-            Tab(text: 'Health'),
-            Tab(text: 'Travel'),
-            Tab(text: 'Science'),
-            Tab(text: 'Fashion'),
-          ],isScrollable: true, dividerColor: Colors.transparent, tabAlignment: TabAlignment.start, )
-          ,Expanded(
-            child: TabBarView(children:
-              NewsCoverageEnum.values.map((e)=> NewsTab(newsCoverage: e)).toList()
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Latest',
+                  ),
+                  InkWell(
+                    child: Text('See all'),
+                    onTap: () {
+                      setState(() {
+                        defaultTab = defaultTab < 4 ? 5 : 0;
+                        print(defaultTab);
+                      });
+                    },
+                  )
+                ],
+              ),
             ),
-          )
-
-        ],),
+            20.ht,
+            TabBar(
+              unselectedLabelStyle: tTheme.bodyMedium,
+              labelStyle: HeadlineBlack,
+              tabs: const [
+                Tab(text: 'All'),
+                Tab(text: 'Sports'),
+                Tab(text: 'Politics'),
+                Tab(text: 'Business'),
+                Tab(text: 'Health'),
+                Tab(text: 'Travel'),
+                Tab(text: 'Science'),
+                Tab(text: 'Fashion'),
+              ],
+              isScrollable: true,
+              dividerColor: Colors.transparent,
+              tabAlignment: TabAlignment.start,
+            ),
+            Expanded(
+              child: TabBarView(
+                  children: NewsCoverageEnum.values
+                      .map((e) => NewsTab(newsCoverage: e))
+                      .toList()),
+            )
+          ],
+        ),
       ),
     );
   }
