f51f40a45e9a4066ca5f3414c9aaee695308c6b9
jagdish <jagdissghimire@gmail.com>
2024-07-26 19:34:58 +0545

    chore: API constants added

---
+++ lib/common/constants/api_constant.dart
@@ -0,0 +1,5 @@
+class ApiConstant {
+  static const String BASE_URL = "https://newsapi.org";
+  static const String DEFAULT_PATH = "/v2/top-headlines";
+  static const String API_KEY = "1e81343d997a4746b543e97772fb70f5";
+}
