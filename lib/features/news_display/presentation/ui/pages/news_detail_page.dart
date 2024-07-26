f2a45bb8ca9360bc70b55d110675e3d5502a8653
jagdish <jagdissghimire@gmail.com>
2024-07-26 11:26:20 +0545

    chore: News app details page implemented

--- lib/features/news_display/presentation/ui/pages/news_detail_page.dart
+++ lib/features/news_display/presentation/ui/pages/news_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:jaggy/features/news_display/presentation/ui/components/news_tile_c.dart';
+import 'package:jaggy/common/extentions/spacer_ext.dart';
+import 'package:jaggy/common/styles/text_styles.dart';
 
 class NewsDetailPage extends StatelessWidget {
   const NewsDetailPage({super.key});
@@ -7,14 +8,64 @@ class NewsDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [NewsTileC(
-        about: "europe",
-        content: "",
-        headline: "Hello i am headlineHello i am headlineHello i am headlineHello i am headlineHello i am headline",
-        newsAgent: "BBC",
-        publishedAt: DateTime.now(),
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9Gvl-o1u8qzqRu_D5UhHh9_-pczQBMluwaA&s",
-      )],),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.share_outlined)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 32,
+                ),
+                5.wd,
+                Column(
+                  children: [
+                    Text(
+                      "BBC News",
+                      style: HeadlineBlack,
+                    ),
+                    Text(
+                      "14 min ago",
+                      style: SimpleThinGrey,
+                    ),
+                  ],
+                ),
+                Spacer(),
+                FilledButton(onPressed: () {}, child: Text('Follow'))
+              ],
+            ),
+            10.ht,
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.purple.shade50,
+                  borderRadius: BorderRadius.circular(10)),
+              height: 250,
+            ),
+            10.ht,
+            Text('Europe'),
+            10.ht,
+            Text(
+              "Ukraine's President Zelensky to BBC: Blood money being paid for Russian oil",
+              style: HeadlineBlack.copyWith(fontSize: 18),
+            ),
+            15.ht,
+            Text(
+                """Ukrainian President Volodymyr Zelensky has accused European countries that continue to buy Russian oil of "earning their money in other people's blood".
+
+In an interview with the BBC, President Zelensky singled out Germany and Hungary, accusing them of blocking efforts to embargo energy sales, from which Russia stands to make up to £250bn (\$326bn) this year.
+
+There has been a growing frustration among Ukraine's leadership with Berlin, which has backed some sanctions against Russia but so far resisted calls to back tougher action on oil sales."""),
+            10.ht,
+            FilledButton(onPressed: () {}, child: Text('Full News Here...'))
+          ],
+        ),
+      ),
     );
   }
 }
