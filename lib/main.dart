40e920b469b3ea86caa0e721469d87bbee3e35a2
jagdish <jagdissghimire@gmail.com>
2024-07-27 12:54:45 +0545

    fix: some ui fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:jaggy/features/news_display/presentation/ui/pages/news_detail_page.dart';
+import 'package:jaggy/features/news_display/presentation/cubit/news_provider_cubit.dart';
+import 'package:jaggy/features/news_display/presentation/ui/pages/news_headlines_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +15,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const NewsDetailPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: BlocProvider(
+          create: (context) => NewsProviderCubit(),
+          child: NewsHeadlinesPage(),
+        ));
   }
 }
