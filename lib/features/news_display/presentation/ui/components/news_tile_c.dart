2745c7f319760a84c3f20c082f763edb4bcadb99
jagdish <jagdissghimire@gmail.com>
2024-07-27 12:56:26 +0545

    chore: some ui adjustment

--- lib/features/news_display/presentation/ui/components/news_tile_c.dart
+++ lib/features/news_display/presentation/ui/components/news_tile_c.dart
@@ -1,26 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:jaggy/common/extentions/spacer_ext.dart';
+import 'package:jaggy/common/helper_functions/error_image.dart';
 import 'package:jaggy/common/styles/text_styles.dart';
+import 'package:jaggy/features/news_display/model/news_model.dart';
+
+import '../../../../../common/helper_functions/date_formatter.dart';
 
 class NewsTileC extends StatelessWidget {
-  final String about;
-  final String image;
-  final String headline;
-  final String newsAgent;
-  final DateTime publishedAt;
-  final String content;
-  const NewsTileC({
-    super.key,
-    required this.about,
-    required this.headline,
-    required this.newsAgent,
-    required this.publishedAt,
-    required this.image,
-    required this.content,
-  });
+  final NewsModel news;
+  const NewsTileC({super.key, required this.news});
 
   @override
   Widget build(BuildContext context) {
+    print(news.urlToImage);
     final mq = MediaQuery.of(context).size.width;
     return Container(
       margin: EdgeInsets.only(top: 15),
@@ -30,21 +22,25 @@ class NewsTileC extends StatelessWidget {
           ClipRRect(
             borderRadius: BorderRadius.circular(10),
             child: Image.network(
-                fit: BoxFit.fill, height: 80, width: 70, '$image'),
+                errorBuilder: (ctx, o, st) => errorImage(),
+                fit: BoxFit.fill,
+                height: 80,
+                width: 70,
+                '${news.urlToImage}'),
           ),
           5.wd,
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                '$about',
+                '${news.author}',
                 style: SimpleThinGrey,
               ),
               2.ht,
               SizedBox(
                   width: mq - 100,
                   child: Text(
-                    "$headline",
+                    "${news.title}",
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
                     style: HeadlineBlack.copyWith(height: 1.2),
@@ -53,7 +49,7 @@ class NewsTileC extends StatelessWidget {
               Row(
                 children: [
                   Text(
-                    "$newsAgent",
+                    "${news.source.name}",
                     style: SimpleBoldBlack,
                   ),
                   20.wd,
@@ -67,7 +63,7 @@ class NewsTileC extends StatelessWidget {
                         color: Colors.grey,
                       ),
                       Text(
-                        ' 14 min ago',
+                        formatDateDifference(news.publishedAt),
                         style: SimpleThinGrey,
                       ),
                     ],
