14c1b0b5fce10fb06ccb563173611c9acb6c7422
jagdish <jagdissghimire@gmail.com>
2024-07-27 17:02:40 +0545

    chore: Env Added for hiding actual api

--- lib/common/constants/api_constant.dart
+++ lib/common/constants/api_constant.dart
@@ -1,5 +1,7 @@
+import 'package:jaggy/common/api_env/api_env.dart';
+
 class ApiConstant {
   static const String BASE_URL = "https://newsapi.org";
   static const String DEFAULT_PATH = "/v2/top-headlines";
-  static const String API_KEY = "1e81343d997a4746b543e97772fb70f5";
+  static const String API_KEY = Env.key;
 }
